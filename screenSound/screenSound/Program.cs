d2ae9416755ff8040cf3532cd6484d1906e82aa8
Gabriel "Syll3x" Ponzoni <109359526+GabrielPonzoni@users.noreply.github.com>
2023-08-17 15:58:49 -0300

    Function ExibirCabecalho created;
    +function for header

--- screenSound/screenSound/Program.cs
+++ screenSound/screenSound/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Screen Sound
 string mensagemBoasVindas = "Bem-Vindo ao Screen Sound por Gabriel Ponzoni!";
-Console.WriteLine(mensagemBoasVindas); //Q1: Ñ se usa print? Porque WriteLine(python se usa esse comando em leitura de arquivo? 
+/*Console.WriteLine(mensagemBoasVindas); //Q1: Ñ se usa print? Porque WriteLine(python se usa esse comando em leitura de arquivo?*/
+
+void ExibirMensagemDeBoasVindas()
+{
+    // Cabecalho do código;
+    Console.WriteLine("*************************");
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine("*************************");
+}
+
+ExibirMensagemDeBoasVindas();
